ebed81f889a0df809d406d6f4c1f1e8bc4b28b8c
Ernane Ferreira <ernane.junior25@gmail.com>
2021-02-05 15:02:15 -0300

    Adicionando arquivos referente a primeira versão do aplicativo de gerenciamento de livros. Bem como, o arquivo README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+Este aplicativo tem como objetivo ajudar na verificação de quais livros estão disponiveis e quais não estão
+por meio de inferface visual desenvolvida no Android Studio em linguagem JAVA.
+
+Projeto realizado com o intuito de obtenção de nota para o termino do curso de Programador para dispositivos moveis do Instituto Metropole Digital (IMD/UFRN).
---
+++ app/src/main/java/com/example/gerenciadordelivros/views/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.gerenciadordelivros.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.gerenciadordelivros.R;
+import com.example.gerenciadordelivros.adapter.LivroAdapter;
+import com.example.gerenciadordelivros.dominio.Livro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<Livro> listaLivros = new ArrayList<>();
+        listaLivros.add(new Livro(1L,"Android para Leigos","Michael Burton","Alta books",false));
+        listaLivros.add(new Livro(2L,"Android para Programadores","Paul J, Deitel","Bookman",true));
+        listaLivros.add(new Livro(3L,"Desenvolvimento para Android","Griffiths, David","Alta books",false));
+        listaLivros.add(new Livro(4L,"Android Base de Dados","Queirós, Ricardo","FCA Editora",true));
+        listaLivros.add(new Livro(5L,"Android em Ação","King, Chris","Elsevier - Campus",false));
+        listaLivros.add(new Livro(6L,"Jogos em Android","Queirós, Ricardo","FCA - Editora",true));
+        listaLivros.add(new Livro(7L,"Android Essencial com Kotlin","Ricardo R.","NOVATEC",false));
+        LivroAdapter livroAdapter = new LivroAdapter(listaLivros,this);
+
+        recyclerView.setAdapter(livroAdapter);
+    }
+}
